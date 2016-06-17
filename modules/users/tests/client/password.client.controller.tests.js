816b34ad87a7698b7c718bf7df211f1c71072108
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-06-17 17:33:44 +0300

    add test

--- modules/users/tests/client/password.client.controller.tests.js
+++ modules/users/tests/client/password.client.controller.tests.js
@@ -52,7 +52,7 @@
       }));
 
       it('should redirect logged in user to home', function() {
-        expect($location.path).toHaveBeenCalledWith('/');
+        expect($location.path).toHaveBeenCalledWith('/h');
       });
     });
 
